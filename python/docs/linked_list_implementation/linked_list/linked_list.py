5fe98ac68f0e789ef1045aa20d95a05d5d992f33
Sam Brindle <brindlejsam@gmail.com>
2022-07-22 17:40:47 -0700

    created my own folder for this assignment without realizing there was one set up already.

---
+++ python/docs/linked_list_implementation/linked_list/linked_list.py
@@ -0,0 +1,34 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        if self.head == None:
+            self.head = Node(value)
+        else:
+            self.value = Node(value)
+
+    def __str__(self):
+        string = "{ "
+        # dummyNode helps you keep track of head of linkedList
+        # dummyNode = self.head
+        # dummyNode.next and so on
+
+        if self.head is None:
+            return "NULL"
+        else:
+            dummy_node = self.head
+            while dummy_node.value.next is not None:
+                string + f"{dummy_node.value}" + " } -> "
+
+            return string + " NULL"
+            # while self.head.value != None:
+            #     string + f"{ self.head.value } -> "
+            # return string + "NULL"
+            # return"{ " + f"{self.head.value}" + " } -> NULL"
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
--- python/docs/linked_list_implementation/tests/test_linked_list.py
+++ python/docs/linked_list_implementation/tests/test_linked_list.py
@@ -1,2 +1,75 @@
-def test_linked_list_instantiation():
-    list =
+import pytest
+from linked_list.linked_list import LinkedList
+
+
+def test_exists():
+    assert LinkedList
+
+
+
+def test_instantiate():
+    assert LinkedList()
+
+
+
+def test_empty_head():
+    linked = LinkedList()
+    assert linked.head is None
+
+
+
+def test_populated_head():
+    linked = LinkedList()
+    linked.insert("apple")
+    assert linked.head.value == "apple"
+
+
+
+def test_to_string_empty():
+    linked_list = LinkedList()
+
+    assert str(linked_list) == "NULL"
+
+
+
+def test_to_string_single():
+    linked_list = LinkedList()
+
+    linked_list.insert("apple")
+
+    assert str(linked_list) == "{ apple } -> NULL"
+
+
+@pytest.mark.skip("TODO")
+def test_to_string_double():
+    linked_list = LinkedList()
+
+    linked_list.insert("apple")
+
+    assert str(linked_list) == "{ apple } -> NULL"
+
+    linked_list.insert("banana")
+
+    assert str(linked_list) == "{ banana } -> { apple } -> NULL"
+
+
+@pytest.mark.skip("TODO")
+def test_includes_true():
+    linked_list = LinkedList()
+
+    linked_list.insert("apple")
+
+    linked_list.insert("banana")
+
+    assert linked_list.includes("apple")
+
+
+@pytest.mark.skip("TODO")
+def test_includes_false():
+    linked_list = LinkedList()
+
+    linked_list.insert("apple")
+
+    linked_list.insert("banana")
+
+    assert not linked_list.includes("cucumber")
