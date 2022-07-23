912c4dd13e3a5a58bb442fdb6e8a764e65f54e05
Sam Brindle <brindlejsam@gmail.com>
2022-07-22 17:38:55 -0700

    created my own folder to do this lab without realizing they had one already

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,37 @@
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        if self.head == None:
+            self.head = Node(value)
+        else:
+            self.value = Node(value)
+
+    def __str__(self):
+        string = "{ "
+        # dummyNode helps you keep track of head of linkedList
+        # dummyNode = self.head
+        # dummyNode.next and so on
+
+        if self.head is None:
+            return "NULL"
+        else:
+            dummy_node = self.head
+            while dummy_node.value.next is not None:
+                string + f"{dummy_node.value}" + " } -> "
+
+            return string + " NULL"
+            # while self.head.value != None:
+            #     string + f"{ self.head.value } -> "
+            # return string + "NULL"
+            # return"{ " + f"{self.head.value}" + " } -> NULL"
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
 
 class TargetError:
