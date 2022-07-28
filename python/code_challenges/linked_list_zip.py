db564d4ca50a239cb9acaef78e04fa97f4c77d2b
Sam Brindle <brindlejsam@gmail.com>
2022-07-27 23:27:42 -0700

    added new zip_list function and added image of whiteboard

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,35 @@
+from data_structures.linked_list import LinkedList, Node, TargetError
+
+
 def zip_lists(a, b):
-    pass
+    list1 = a.head
+    list2 = b.head
+    list3 = LinkedList()
+    switch = True
+
+    if list1:
+        list3.insert(list1.value)
+        list1 = list1.next
+    elif list2:
+        list3.insert(list2.value)
+        list2 = list2.next
+
+    current = list3.head
+
+    while list1 is not None or list2 is not None:
+        if switch and list2 is not None:
+            list3.insert_after(current.value, list2.value)
+            list2 = list2.next
+            current = current.next
+
+        if not switch and list1 is not None:
+            list3.insert_after(current.value, list1.value)
+            list1 = list1.next
+            current = current.next
+
+        if switch:
+            switch = False
+        else:
+            switch = True
+
+    return list3
