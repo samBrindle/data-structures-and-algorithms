f2d4b4af8607eea95e872d1a6014a166efa493f5
Sam Brindle <brindlejsam@gmail.com>
2022-08-01 14:00:24 -0700

    added new class and required methods, passing all test

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,27 @@
 from data_structures.stack import Stack
+from data_structures.invalid_operation_error import InvalidOperationError
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def enqueue(self, value):
+        if self.stack1.is_empty():
+            self.stack1.push(value)
+            return
+        else:
+            while not self.stack1.is_empty():
+                popped = self.stack1.pop()
+                self.stack2.push(popped)
+
+            self.stack1.push(value)
+
+            while not self.stack2.is_empty():
+                popped = self.stack2.pop()
+                self.stack1.push(popped)
+
+    def dequeue(self):
+        return self.stack1.pop()
+
