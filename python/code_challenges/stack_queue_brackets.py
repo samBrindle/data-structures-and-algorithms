09faa38bfe8594cd99488c0668addd1709baaec4
Sam Brindle <brindlejsam@gmail.com>
2022-08-03 21:08:17 -0700

    added code along with whiteboard

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,43 @@
 from data_structures.queue import Queue
+import re
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    regex = r"[{|(|[|}|)|\]]"
+    prev_value = None
+
+    check_list = re.findall(regex, string)
+
+    if check_list[0] == ']' or check_list[0] == '}' or check_list[0] == ')':
+        return False
+    for char in check_list:
+        if prev_value is None:
+            if char == '{' or char == '[' or char == '(':
+                prev_value = char
+        else:
+            # check if new opening bracket, if so recursive
+            if (char == '{' or char == '[' or char == '(') and\
+                    (prev_value == '{' or prev_value == '[' or prev_value == '('):
+                new_list = check_list
+                new_list.pop(0)
+                new_string = ''.join(new_list)
+                if not multi_bracket_validation(new_string):
+                    return True
+
+            if prev_value == '{':
+                if char == ']' or char == ')':
+                    return False
+            if prev_value == '[':
+                if char == '}' or char == ')':
+                    return False
+            if prev_value == '(':
+                if char == ']' or char == '}':
+                    return False
+
+            prev_value = char
+
+            if char == '}' or char == ']' or char == ')':
+                prev_value = None
+    return True
+
+
---
+++ python/docs/stack_queue_brackets/README.md
@@ -0,0 +1,2 @@
+![Multi Bracket Validation Code1](multi_bracket_val_code1.png)
+![Multi Bracket Validation Code2](multi_bracket_val_code2.png)
