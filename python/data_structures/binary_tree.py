137322a0494bed6ad143714cc088a72f848dbbcb
Sam Brindle <brindlejsam@gmail.com>
2022-08-05 16:37:33 -0700

    added code to binary_tree and binary_search files. Passing all tests

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -3,14 +3,71 @@ class BinaryTree:
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, root=None):
+        self.root = root
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+        values = []
+
+        def stroll(root):
+            if root is None:
+                return
+
+            values.append(root.value)
+
+            stroll(root.left)
+
+            stroll(root.right)
+
+        stroll(self.root)
+
+        return values
+
+    def in_order(self):
+        values = []
+
+        def stroll(root):
+            if root is None:
+                return
+
+            # left
+            stroll(root.left)
+
+            # root
+            values.append(root.value)
+
+            # right
+            stroll(root.right)
+
+        stroll(self.root)
+
+        return values
+
+    def post_order(self):
+        values = []
+
+        def stroll(root):
+            if root is None:
+                return
+
+            # left
+            stroll(root.left)
+
+            # right
+            stroll(root.right)
+
+            # root
+            values.append(root.value)
+
+        stroll(self.root)
+
+        return values
 
 
 class Node:
-    pass
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
