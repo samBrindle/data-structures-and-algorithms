c306d02048ae7e69212a8049f340f9607ad9b6fe
Sam Brindle <brindlejsam@gmail.com>
2022-08-08 14:21:48 -0700

    added code to pass tests!

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,21 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    queue = Queue()
+    tree_list = []
+    root = tree.root
+    queue.enqueue(root)
+
+    if root is None:
+        return tree_list
+    while not queue.is_empty():
+        curr = queue.dequeue()
+        tree_list.append(curr.value)
+        if curr.left:
+            queue.enqueue(curr.left)
+        if curr.right:
+            queue.enqueue(curr.right)
+
+    return tree_list
