5684eaaf64a2f5c82b71fd325bf305c6fd936335
Sam Brindle <brindlejsam@gmail.com>
2022-09-05 13:39:03 -0700

    added functionality

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -5,12 +5,39 @@ class Graph:
 
     def __init__(self):
         # initialization here
-        pass
+        self.adjacency_list = {}
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, value):
+        vertex = Vertex(value)
+
+        self.adjacency_list[vertex] = []
+
+        return vertex
+
+    def size(self):
+        return len(self.adjacency_list)
+
+    def add_edge(self, start, end, weight=0):
+        edge = Edge(end, weight)
+
+        if end not in self.adjacency_list:
+            raise KeyError
+
+        self.adjacency_list[start].append(edge)
+
+    def get_nodes(self):
+        return self.adjacency_list.keys()
+
+    def get_neighbors(self, vertex):
+        return self.adjacency_list[vertex]
 
 
 class Vertex:
-    pass
+    def __init__(self, value):
+        self.value = value
+
+
+class Edge:
+    def __init__(self, vertex, weight):
+        self.vertex = vertex
+        self.weight = weight
